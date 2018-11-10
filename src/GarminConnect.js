c4eb0041837b12c6a98fba4e5b864f9275b813b2
Joey Jan <overflow636@gmail.com>
2018-11-10 16:26:34 -0500

    update for garmin site changes

--- src/GarminConnect.js
+++ src/GarminConnect.js
@@ -1,7 +1,7 @@
-const request               = require('request');
-const garminConnectLogin    = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&webhost=olaxpw-connect04&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false';
-const garminConnectPostAuth = 'https://connect.garmin.com/post-auth/login?';
-const garminConnectWeights  = 'https://connect.garmin.com/modern/proxy/userprofile-service/userprofile/personal-information/weightWithOutbound/filterByDay?from=0&until=9999999999999&_=1478359936529';
+const request = require('request');
+const garminConnectLogin = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&webhost=olaxpw-connect04&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false';
+const garminConnectPostAuth = 'https://connect.garmin.com/modern/?';
+const garminConnectWeights = 'https://connect.garmin.com/modern/proxy/userprofile-service/userprofile/personal-information/weightWithOutbound/filterByDay?from=0&until=9999999999999&_=1478359936529';
 
 module.exports = function getGarminWeights(username, password) {
   return new Promise((resolve, reject) => {
@@ -15,13 +15,13 @@ module.exports = function getGarminWeights(username, password) {
       }
 
       requestJar({
-        uri   : garminConnectLogin,
-        form  : {
+        uri: garminConnectLogin,
+        form: {
           username,
           password,
-          'embed'              : 'true',
-          'lt'                 : 'e1s1',
-          '_eventId'           : 'submit',
+          'embed': 'true',
+          'lt': 'e1s1',
+          '_eventId': 'submit',
           'displayNameRequired': 'false'
         },
         method: 'POST'
@@ -34,25 +34,19 @@ module.exports = function getGarminWeights(username, password) {
         xhr.headers['set-cookie'].forEach(function (cookie) {
           if (cookie.indexOf('CASTGC') === 0) {
             loginSuccess = true;
-            var ticket   = cookie.substr(11, cookie.indexOf(';') - 11);
+            var ticket = cookie.substr(11, cookie.indexOf(';') - 11);
 
             requestJar(garminConnectPostAuth + 'ticket=ST-0' + ticket, function (err, xhr) {
               if (err || xhr.statusCode != 200) {
                 return reject('Could not get post auth page ' + garminConnectPostAuth + 'ticket=ST-0' + ticket + ' | result: ' + (xhr ? xhr.statusCode : 'noxhr'));
               }
 
-              requestJar("https://connect.garmin.com/legacy/session", function (err, xhr) {
+              requestJar(garminConnectWeights, function (err, xhr, body) {
                 if (err || xhr.statusCode != 200) {
-                  return reject('Could not get legacy session');
+                  return reject('Could not get weight data', xhr.statusCode);
                 }
 
-                requestJar(garminConnectWeights, function (err, xhr, body) {
-                  if (err || xhr.statusCode != 200) {
-                    return reject('Could not get weight data', xhr.statusCode);
-                  }
-
-                  resolve(JSON.parse(body));
-                });
+                resolve(JSON.parse(body));
               });
             });
           }
